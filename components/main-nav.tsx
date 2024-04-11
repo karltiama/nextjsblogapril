0f7354e574f926c9dee0f135688b64849502c01d
Karl Tiama <karltiama@gmail.com>
2024-04-10 22:46:55 -0400

    feat: make header and mobile nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -18,7 +18,7 @@ export function MainNav() {
       <Link 
         href="/blog" 
         className={cn(
-          "text-sm font-medium transition-colors hover:text-primary", 
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block", 
           pathname === "/blog" ? "text-foreground" : "text-foreground/60"
         )}
         >
@@ -27,7 +27,7 @@ export function MainNav() {
       <Link 
         href="/projects" 
         className={cn(
-          "text-sm font-medium transition-colors hover:text-primary", 
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block", 
           pathname === "/projects" ? "text-foreground" : "text-foreground/60"
         )}
         >
@@ -36,7 +36,7 @@ export function MainNav() {
       <Link 
         href="/about" 
         className={cn(
-          "text-sm font-medium transition-colors hover:text-primary", 
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block", 
           pathname === "/about" ? "text-foreground" : "text-foreground/60"
         )}
         >
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,6 +4,7 @@ import Link  from "next/link";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
@@ -14,7 +15,7 @@ export function SiteHeader() {
             <nav className="flex items-center">
               <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
                 <div className={cn(buttonVariants({ variant: "ghost"}),
-                "w-10 px-0"
+                "w-10 px-0 hidden sm:inline-flex"
                 )}
               >
                 <Icons.gitHub className="h-4 w-4" />
@@ -23,13 +24,14 @@ export function SiteHeader() {
               </Link>
               <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
                 <div className={cn(buttonVariants({ variant: "ghost"}),
-                "w-10 px-0"
+                "w-10 px-0 hidden sm:inline-flex"
                 )}
               >
                 <Icons.twitter className="h-4 w-4" />
                 <span className="sr-only">Twitter</span>
                 </div>
               </Link>
+              <MobileNav />
             </nav>
           </div>
         </div>
