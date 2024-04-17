70e3e549102df258965a48db14f35a29be35acd6
Karl Tiama <karltiama@gmail.com>
2024-04-17 11:39:24 -0400

    feat: blog post

--- components/callout.tsx
+++ components/callout.tsx
@@ -14,7 +14,7 @@ export function Callout({
 	return (
 		<div
 			className={cn(
-				"my-6 items-start rounded-md border border-l-4 p-4 w-full dark:max-w-none",
+				"my-6 items-start rounded-md border border-l-8 p-4 w-full dark:max-w-none",
 				{
 					"border-red-900 bg-red-50 dark:prose": type === "danger",
 					"border-yellow-900 bg-yellow-50 dark:prose": type === "warning",
