b628a318125073d6b1069956f97e6e9053357eda
Karl Tiama <karltiama@gmail.com>
2024-06-17 22:36:58 -0400

    refactor: Update home page layout and images

--- components/callout.tsx
+++ components/callout.tsx
@@ -17,7 +17,7 @@ export function Callout({
 				"my-6 items-start rounded-md border border-l-8 p-4 w-full dark:max-w-none",
 				{
 					"border-red-900 bg-red-50 dark:prose": type === "danger",
-					"border-yellow-900 bg-yellow-50 dark:prose": type === "warning",
+					"border-yellow-600 bg-yellow-50 dark:prose": type === "warning",
 				}
 			)}
 			{...props}>
