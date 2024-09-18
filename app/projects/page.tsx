7eebfde9e57aa4229946a45f6f28945aa1fb281a
Karl Tiama <karltiama@gmail.com>
2024-09-18 16:10:15 -0400

    Refactor: adjust spacing of Projects page to be consitent with other pages

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,15 +3,13 @@ import ProjectList from "@/components/project-list"; // Adjust the path as neede
 export default async function ProjectsPage() {
 	return (
 		<div className="container max-w-4xl py-6 lg:py-10">
-			<div className="flex flex-col items-start md:flex-row md:justify-between md:gap-8">
-				<div className="flex-1 space-y-4">
-					<h1 className="inline-block font-black text-4xl lg:text-5xl">
-						Projects
-					</h1>
-					<p className="text-xl text-muted-foreground">
-						My personal projects and experiments.
-					</p>
-				</div>
+			<div className="flex flex-col items-start gap-4">
+				<h1 className="font-black text-3xl md:text-4xl lg:text-5xl">
+					Projects
+				</h1>
+				<p className="text-xl text-muted-foreground">
+					My personal projects and experiments.
+				</p>
 			</div>
 
 			{/* Pass the projects array to the ProjectList component */}
